396744decf2ab82407ac4eb90c7e9d82ad7c1677
AlanJava <763162583@qq.com>
2022-10-28 17:31:01 +0800

    ad

---
+++ hexo-fluid/scripts/ad.js
@@ -0,0 +1,3 @@
+hexo.extend.injector.register('head_begin', '    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3579759224836361"\n' +
+    '            crossorigin="anonymous"></script>\n' +
+    '\n', 'default');
