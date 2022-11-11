f953c5c37c4894a9cd8e6fc5a55f7bfe7c44a8d4
AlanJava <763162583@qq.com>
2022-11-11 10:49:00 +0800

    ad

--- hexo-fluid/scripts/ad.js
+++ hexo-fluid/scripts/ad.js
@@ -1,3 +1,2 @@
-hexo.extend.injector.register('head_begin', '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3579759224836361"\n' +
-    '     crossorigin="anonymous"></script>' +
-    '\n', 'default');
+hexo.extend.injector.register('head_begin',"<script async src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3579759224836361\"\n" +
+    "     crossorigin=\"anonymous\"></script>", 'default');
